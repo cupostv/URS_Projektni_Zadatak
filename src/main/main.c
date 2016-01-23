a4f24d2c479abf3b6cb3c4f6dfa8160d0bff8693
cupostv <cupostv@gmail.com>
2016-01-23 16:03:25 +0100

    Added enumeration ErrorCode in palindrome.h and written basic main function

---
+++ src/main/main.c
@@ -0,0 +1,6 @@
+#include "../palindrome/palindrome.h"
+
+int32_t main (int32_t argc, uint8_t* argv[])
+{
+	return NO_ERROR;
+}
---
+++ src/palindrome/palindrome.h
@@ -0,0 +1,14 @@
+#ifndef PALINDROME_H
+#define PALINDROME_H
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <errno.h>
+
+typedef enum _ErrorCode
+{
+	NO_ERROR = 0,
+	ERROR = 1
+} ErrorCode;
+
+#endif
